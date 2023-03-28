04f52f89097ff2ad0649f2e8d6dabe6a46b8790b
siemens <sema.perstnev.01@mail.ru>
2023-03-28 20:14:23 +0300

    first commit

---
+++ src/main/java/EmployeeBook/my/employeebookdemo/EmployeebookDemoApplication.java
@@ -0,0 +1,13 @@
+package EmployeeBook.my.employeebookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeebookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeebookDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EmployeeBook/my/employeebookdemo/EmployeebookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package EmployeeBook.my.employeebookdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
